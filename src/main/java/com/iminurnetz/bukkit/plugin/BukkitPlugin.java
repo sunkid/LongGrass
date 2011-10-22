6aae19b31739d4dc9a0ba8eaad413471f9e3823e
sunkid <sunkid@iminurnetz.com>
2011-10-22 13:48:17 -0700

    Squashed 'base/' changes from 14fd3c7..4da5e57
    4da5e57 version 2.0
    a8ddbb1 version 1.9
    b399a32 version 1.8
    ee50555 tested version
    e5e7540 second attempt
    705214e first attempt
    dafe690 Merge branch 'master' of github.com:sunkid/BaseBukkitPlugin
    730e6dd correct file
    
    git-subtree-dir: base
    git-subtree-split: 4da5e57a15fe8d689a8cee76b818e5f2e95b7d38

--- src/main/java/com/iminurnetz/bukkit/plugin/BukkitPlugin.java
+++ src/main/java/com/iminurnetz/bukkit/plugin/BukkitPlugin.java
@@ -23,30 +23,48 @@
  */
 package com.iminurnetz.bukkit.plugin;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.iminurnetz.bukkit.plugin.util.MessageUtils;
 import com.iminurnetz.bukkit.plugin.util.PluginLogger;
+import com.iminurnetz.util.DownloadUtils;
 
 public abstract class BukkitPlugin extends JavaPlugin {
-	protected PluginLogger logger;
+    private static final String BASE_BUKKIT_PLUGIN = "BaseBukkitPlugin";
 
-	protected int MIN_SERVER_VERSION = 400;
-	protected int MAX_SERVER_VERSION = Integer.MAX_VALUE;
+    protected PluginLogger logger;
+
+    protected int MIN_SERVER_VERSION = 400;
+    protected int MAX_SERVER_VERSION = Integer.MAX_VALUE;
 
     private PluginDescriptionFile description;
 
-	public BukkitPlugin() {
+    private static final String HOME_URL = "http://www.iminurnetz.com/mcStats.cgi";
+
+    public static final String REPOSITORY = "https://raw.github.com/sunkid/@project@/master/release/";
+
+    public BukkitPlugin() {
         try {
             InputStream is = getClass().getResourceAsStream("/plugin.yml");
             description = new PluginDescriptionFile(is);
@@ -55,22 +73,22 @@ public abstract class BukkitPlugin extends JavaPlugin {
         }
 
         logger = new PluginLogger(this);
-		logger.log("initialized");
-	}
-	
-	public PluginLogger getLogger() {
-		return logger;
-	}
+        logger.log("initialized");
+    }
 
-	// simple shortcut
-	public void log(String msg) {
-		getLogger().log(msg);
-	}
+    public PluginLogger getLogger() {
+        return logger;
+    }
 
-	// simple shortcut
-	public void log(Level level, String msg) {
-		getLogger().log(level, msg);
-	}
+    // simple shortcut
+    public void log(String msg) {
+        getLogger().log(msg);
+    }
+
+    // simple shortcut
+    public void log(Level level, String msg) {
+        getLogger().log(level, msg);
+    }
 
     // simple shortcut
     public void log(Level level, String msg, Exception e) {
@@ -89,77 +107,257 @@ public abstract class BukkitPlugin extends JavaPlugin {
 
     public String getName() {
         return getDescription().getName();
-	}
+    }
 
-	public String getVersion() {
+    public String getVersion() {
         return getDescription().getVersion();
-	}
-	
-	public int getServerVersion() {
-	    String[] sv = getServer().getVersion().split("-");
-	    int version = -1;
-	    try {
-	        version = Integer.valueOf(sv[3]);
-	    } catch (Exception e) {
-	        log("Unfamiliar version string " + getServer().getVersion());
-	    }
-	    
-	    return version;
-	}
+    }
 
-	public String getFullMessagePrefix() {
-		return getFullMessagePrefix(ChatColor.WHITE);
-	}
-	
-	public String getFullMessagePrefix(ChatColor color) {
-		return MessageUtils.colorize(color, "[" + getName() + " " + getVersion() + "] ");
-	}
-	
-	public String getMessagePrefix() {
-		return "[" + getName() + "] ";
-	}
-	
-	public String getMessagePrefix(ChatColor color) {
-		return MessageUtils.colorize(color, getMessagePrefix());
-	}
-
-	@Override
-	public void onDisable() {
-		getLogger().log("un-loaded");
-	}
-	
-	@Override
-	public final void onEnable() {
-	    int serverVersion = getServerVersion();
+    public int getServerVersion() {
+        String[] sv = getServer().getVersion().split("-");
+        int version = -1;
         try {
-            if (serverVersion > 0 &&
-                (serverVersion < getMinimumServerVersion() || serverVersion > getMaximumServerVersion())) {
+            version = Integer.valueOf(sv[3]);
+        } catch (Exception e) {
+            log("Unfamiliar version string " + getServer().getVersion());
+        }
 
-                throw new UnsupportedServerVersionException(
-                        "This plugin only supports server versions "
-                                + getMinimumServerVersion() + " to "
-                                + getMaximumServerVersion());
+        return version;
+    }
+
+    public String getFullMessagePrefix() {
+        return getFullMessagePrefix(ChatColor.WHITE);
+    }
+
+    public String getFullMessagePrefix(ChatColor color) {
+        return MessageUtils.colorize(color, "[" + getName() + " " + getVersion() + "] ");
+    }
+
+    public String getMessagePrefix() {
+        return "[" + getName() + "] ";
+    }
+
+    public String getMessagePrefix(ChatColor color) {
+        return MessageUtils.colorize(color, getMessagePrefix());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().log("un-loaded");
+    }
+
+    @Override
+    public final void onEnable() {
+        int serverVersion = getServerVersion();
+        try {
+            if (serverVersion > 0 && (serverVersion < getMinimumServerVersion() || serverVersion > getMaximumServerVersion())) {
+
+                throw new UnsupportedServerVersionException("This plugin only supports server versions " + getMinimumServerVersion() + " to " + getMaximumServerVersion());
             }
 
             if (serverVersion > 0) {
                 log("Server version compatibility check succeeded");
             }
 
+            if (getServer().getPluginManager().getPlugin(BASE_BUKKIT_PLUGIN) == null) {
+                updateAndLoadBaseBukkitPlugin();
+            }
+
             enablePlugin();
+
+            if (!getName().equals(BASE_BUKKIT_PLUGIN)) {
+                Configuration config = getConfig();
+                if (!config.getBoolean("settings.disable-stats", false)) {
+                    getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
+                        public void run() {
+                            postUsage();
+                        }
+                    });
+                }
+
+                if (!config.getBoolean("settings.disable-updates", false)) {
+                    getServer().getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
+                        public void run() {
+                            File jarFile = new File(getDataFolder().getParentFile(), getName() + ".jar");
+                            try {
+                                checkAndUpdateJarFile(jarFile, true);
+                            } catch (Exception e) {
+                                log(Level.SEVERE, "Cannot check for or install latest version", e);
+                            }
+                        }
+                    });
+                }
+            }
+
         } catch (Exception e) {
             log("Error enabling! ABORTED", e);
             this.setEnabled(false);
         }
-	}
-	
-	protected int getMinimumServerVersion() { return MIN_SERVER_VERSION; }
-    protected int getMaximumServerVersion() { return MAX_SERVER_VERSION; }
-    
-    /**
-     * This method will be called when the onEnable() method is called.
-     * @throws Exception 
-     */
-    public abstract void enablePlugin() throws Exception;
+    }
+
+    public void postUsage() {
+        log("Sending anonymous usage data...");
+        log("This can be disabled by setting 'settings.disable-stats' to true in config.yml");
+
+        HashMap<String, String> values = new HashMap<String, String>();
+        String ip = getServer().getIp();
+        if (ip.equals("")) {
+            ip = "*";
+        }
+
+        values.put("ip", ip);
+        values.put("port", String.valueOf(getServer().getPort()));
+        values.put("plugin", getName());
+        values.put("version", getVersion());
+
+        try {
+            log(DownloadUtils.post(HOME_URL, values));
+        } catch (IOException e) {
+            // ignored
+        }
+    }
+
+    public void updateAndLoadBaseBukkitPlugin() throws Exception {
+        Plugin plugin;
+
+        File basePluginJarFile = new File(getDataFolder(), BASE_BUKKIT_PLUGIN + ".jar");
+        checkAndUpdateJarFile(basePluginJarFile, false);
+
+        PluginManager pm = getServer().getPluginManager();
+        plugin = pm.loadPlugin(basePluginJarFile);
+        pm.enablePlugin(plugin);
+
+    }
+
+    private void checkAndUpdateJarFile(File jarFile, boolean isPlugin) throws Exception {
+        boolean checkServer = checkTimeStamp(isPlugin);
+
+        String installedVersion = getPluginVersionFromJar(jarFile);
+        if (installedVersion.equals("0")) {
+            checkServer = true;
+        }
+
+        if (checkServer) {
+            String name = jarFile.getName().replace(".jar", "");
+
+            log("Checking for lastest version of " + name);
+            if (isPlugin) {
+                log("Automatic updates can be disabled by setting 'settings.disable-updates' to false in config.yml");
+            }
+
+            String latestVersion = getLatestVersionFromRepository(name);
+
+            if (!latestVersion.equals(installedVersion)) {
+                log("Latest version " + latestVersion + " is newer than the installed version!");
+
+                // not sure this is necessary, as SimplePluginManager seems to move files automatically
+                // at least it'll create the folder
+                if (isPlugin) {
+                    jarFile = new File(getServer().getUpdateFolderFile(), jarFile.getName());
+                    if (!jarFile.getParentFile().exists()) {
+                        jarFile.getParentFile().mkdir();
+                    } else {
+                        String updateVersion = getPluginVersionFromJar(jarFile);
+                        if (updateVersion.equals(getVersion())) {
+                            log("The latest version has already been downloaded to " + jarFile.getAbsolutePath());
+                            return;
+                        }
+                    }
+                }
+
+                URL jarUrl = new URL(getRepository(name) + jarFile.getName());
+                DownloadUtils.download(logger, jarUrl, jarFile);
+                log("The latest version was downloaded to " + jarFile.getAbsolutePath());
+                if (isPlugin) {
+                    log("The update will automatically be installed upon the next server restart!");
+                }
+            }
+
+            if (isPlugin) {
+                try {
+                    if (!getTimeStampFile().setLastModified((new Date()).getTime())) {
+                        throw new Exception("Setting last modified time stamp did not succeed!");
+                    }
+                } catch (Exception e) {
+                    log(Level.SEVERE, "Cannot update time stamp", e);
+                }
+            }
+        }
+    }
+
+    private boolean checkTimeStamp(boolean create) {
+        return checkTimeStamp(24, create);
+    }
+
+    private boolean checkTimeStamp(int interval, boolean create) {
+        File dataFolder = getDataFolder();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+
+        File timeStamp = getTimeStampFile();
+        boolean checkServer = true;
+        if (!timeStamp.exists() && create) {
+            try {
+                BufferedWriter out = new BufferedWriter(new FileWriter(timeStamp));
+                out.write("remove this file to force the version checks for BaseBukkitPlugin and " + getName() + "\n");
+                out.close();
+            } catch (IOException e) {
+                log(Level.SEVERE, "Cannot create time stamp", e);
+                return true;
+            }
+        } else if (create) {
+            Date lastModified = new Date(timeStamp.lastModified());
+            Calendar yesterday = Calendar.getInstance();
+            yesterday.add(Calendar.HOUR, -interval);
+
+            checkServer = lastModified.before(yesterday.getTime());
+        }
+        return checkServer;
+    }
+
+    private String getPluginVersionFromJar(File pluginJarFile) {
+        String pluginVersion = "0";
+
+        if (pluginJarFile.exists()) {
+            try {
+                JarFile jarFile = new JarFile(pluginJarFile);
+                JarEntry entry = jarFile.getJarEntry("plugin.yml");
+                InputStream stream = jarFile.getInputStream(entry);
+
+                PluginDescriptionFile desc = new PluginDescriptionFile(stream);
+                pluginVersion = desc.getVersion();
+                stream.close();
+                jarFile.close();
+            } catch (Exception e) {
+                log(Level.SEVERE, "Cannot check plugin version for " + pluginJarFile.getName(), e);
+            }
+        }
+
+        return pluginVersion;
+    }
+
+    public static String getLatestVersionFromRepository(String project) throws IOException {
+        URL versionUrl = new URL(getRepository(project) + "version.txt");
+        String latestVersion = DownloadUtils.readURL(versionUrl);
+        return latestVersion.trim();
+    }
+
+    private static String getRepository(String project) {
+        return REPOSITORY.replace("@project@", project);
+    }
+
+    private File getTimeStampFile() {
+        return new File(getDataFolder(), "lastVersionCheck.txt");
+    }
+
+    protected int getMinimumServerVersion() {
+        return MIN_SERVER_VERSION;
+    }
+
+    protected int getMaximumServerVersion() {
+        return MAX_SERVER_VERSION;
+    }
 
     public void writeResourceToDataFolder(String in) {
         writeResourceToDataFolder(in, in);
@@ -195,4 +393,11 @@ public abstract class BukkitPlugin extends JavaPlugin {
     public File getDataFile(String name) {
         return new File(getDataFolder(), name);
     }
+
+    /**
+     * This method will be called when the onEnable() method is called.
+     * 
+     * @throws Exception
+     */
+    public abstract void enablePlugin() throws Exception;
 }
